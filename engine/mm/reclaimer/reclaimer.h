d133f049a3280a9ccd8fa8aa3d16554be05adf34
davisson2003 <davisson2003@gmail.com>
2017-02-01 12:48:43 +0800

    Project Initialize

---
+++ engine/mm/reclaimer/reclaimer.h
@@ -0,0 +1,5 @@
+namespace zoo {
+namespace mm {
+}
+
+}
--- tools/cmdline/zcm.cc
+++ tools/cmdline/zcm.cc
@@ -1,5 +1,11 @@
-namespace zoo {
-class zcm {
+#include "type_definition.h"
+using namespace std;
 
-};
+namespace zoo {
+  static int zcmd(int argc, char** argv) {
+   
+  }
+}
+int main (int argc, char* argv[]) {
+   return zoo::zcmd(argc, argv);  
 }
