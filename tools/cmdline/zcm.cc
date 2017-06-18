c46716793e3b120117c9b0c1483378601c92dc15
Zang Lin <davisson2003@gmail.com>
2017-06-18 22:55:55 +0800

    update cmdline

--- tools/cmdline/zcm.cc
+++ tools/cmdline/zcm.cc
@@ -3,9 +3,26 @@ using namespace std;
 
 namespace zoo {
   static int zcmd(int argc, char** argv) {
-   
+    CoreEngine* engine;
+    ProcessArgsForZcmd(&argc, &argv);
+    Logger* Logger = Logger::getInstance();
+    if (!engine = CoreEngine::Create(argc, argv)) {
+      Logger->w("Fail to Create CoreEngine!");
+      return -1;
+    }
+    // char sig = signature of main
+    int exec_result = InvokeEntryFunction(engine, "main", sig);
+
+    if (!exec_result) {
+      Logger->e("Error when Invoke Entry Function");
+    }
+
+    DestroyEngine();
+    return exec_result;
+
   }
-}
+} // namespace zoo
+
 int main (int argc, char* argv[]) {
    return zoo::zcmd(argc, argv);  
 }
